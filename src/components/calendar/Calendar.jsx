365085e61b4e107580e8a10f45a1968cd3677f3c
Kristine Leetberg <kr1stine@ut.ee>
2021-01-29 16:50:54 +0200

    Loading state is managed by Redux

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,35 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchHolidays } from "../../utils/api";
 
 import styles from "./Calendar.module.css";
 import {
+  selectLoading,
   requestHolidays,
   selectHolidays,
   selectWeekStartDate,
 } from "./calendarSlice";
 
 const Calendar = () => {
+  const loading = useSelector(selectLoading);
   const holidays = useSelector(selectHolidays);
   const weekStartDate = useSelector(selectWeekStartDate);
 
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState([]);
 
   useEffect(() => {
     dispatch(requestHolidays("2019-02-01", "2019-02-28"));
   }, []);
 
   return (
-    <section className={styles.weekGrid}>
-      <div className={styles.day}>E</div>
-      <div className={styles.day}>E</div>
-      <div className={styles.day}>E</div>
-      <div className={styles.day}>E</div>
-      <div className={styles.day}>E</div>
-      <div className={styles.day}>E</div>
-      <div className={styles.day}>E</div>
-    </section>
+    <Fragment>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <section className={styles.weekGrid}>
+          <div className={styles.day}>E</div>
+          <div className={styles.day}>E</div>
+          <div className={styles.day}>E</div>
+          <div className={styles.day}>E</div>
+          <div className={styles.day}>E</div>
+          <div className={styles.day}>E</div>
+          <div className={styles.day}>E</div>
+        </section>
+      )}
+    </Fragment>
   );
 };
 
--- src/components/calendar/calendarSlice.js
+++ src/components/calendar/calendarSlice.js
@@ -5,28 +5,52 @@ import { findDefaultWeekStartDate, findNextDateByDay } from "utils/helpers";
 export const calendarSlice = createSlice({
   name: "calendar",
   initialState: {
+    loading: true,
     holidays: [],
     weekStartDate: findDefaultWeekStartDate(),
+    earliestDateFetched: null,
+    latestDateFetched: null,
   },
   reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
     setWeekStartDay: (state, action) => {
       state.weekStartDate = findNextDateByDay(action.payload);
     },
     addHolidays: (state, action) => {
       state.holidays = state.holidays.concat(action.payload);
     },
+    setEarliestDateFetched: (state, action) => {
+      state.earliestDateFetched = action.payload;
+    },
+    setLatestDateFetched: (state, action) => {
+      state.earliestDateFetched = action.payload;
+    },
   },
 });
 
-export const { setWeekStartDay, addHolidays } = calendarSlice.actions;
+export const {
+  setWeekStartDay,
+  addHolidays,
+  setEarliestDateFetched,
+  setLatestDateFetched,
+  setLoading,
+} = calendarSlice.actions;
 
 export const requestHolidays = (startDate, endDate) => (dispatch) => {
   fetchHolidays(startDate, endDate).then((resp) => {
     dispatch(addHolidays(resp.holidays));
+    dispatch(setLoading(false));
   });
 };
 
+export const selectLoading = (state) => state.calendar.loading;
 export const selectHolidays = (state) => state.calendar.holidays;
 export const selectWeekStartDate = (state) => state.calendar.weekStartDate;
+export const selectEarliestDateFetched = (state) =>
+  state.calendar.earliestDateFetched;
+export const selectLatestDateFetched = (state) =>
+  state.calendar.latestDateFetched;
 
 export default calendarSlice.reducer;
