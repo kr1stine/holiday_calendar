4fa24b598e7c790d980e4b710689bc1e4fcd154c
Kristine Leetberg <kr1stine@ut.ee>
2021-01-31 13:53:15 +0200

    Added logos to avoid breakage in manifest.json

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -86,6 +86,7 @@ const Calendar = () => {
   // Call handleDayChanged on first load
   useEffect(() => {
     handleDayChanged(displayStartDay);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
